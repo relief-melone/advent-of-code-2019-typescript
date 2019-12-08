1faeb054bb1d13f9cca095372f848d5d9a3b89ad
relief-melone <christian.debiasi@hotmail.de>
2019-12-08 11:12:29 +0100

    Day 8 complete

--- test/test.08.01.ts
+++ test/test.08.01.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { calculateNrOfLayers, splitInputIntoLayers, countDigitsInLayer } from '../src/08.01/exec';
 
-describe.only('Day 8 - Second Puzzle', () => {
+describe('Day 8 - Second Puzzle', () => {
   it('return number of layers', () => {
     expect(calculateNrOfLayers([1,2,3,4,5,6,7,8,9,0,1,2],3,2)).to.equal(2);
   });
---
+++ test/test.08.02.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { mergeLayers, layerToGrid } from '../src/08.02/exec';
+
+describe.only('Day 8 - Second Puzzle', () => {
+  it('correctly merges layers', () => {
+    expect(mergeLayers([
+      [0,2,2,2],
+      [1,1,2,2],
+      [2,2,1,2],
+      [0,0,0,0]
+    ])).to.deep.equal(
+      [0,1,1,0]
+    );
+  });
+
+  it('correctly converts a layer into a grid', () => {
+    expect(layerToGrid([1,2,3,4,5,6],3,2)).to.deep.equal([
+      [1,2,3],
+      [4,5,6]
+    ]);
+  });
+  
+});
