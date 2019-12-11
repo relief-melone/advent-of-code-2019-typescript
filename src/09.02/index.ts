a6ec22e246b7af19b34cd851d48e9413795382af
de Biasi Christian <christian.de-biasi@bmw.de>
2019-12-11 15:32:44 +0100

    Day 9.2 Complete

---
+++ src/09.02/index.ts
@@ -0,0 +1,6 @@
+import { solveInput } from '../09.01/exec';
+import Input from '../shared/input';
+
+const input = new Input('input.09.01.txt');
+
+console.log(`COORDINATES: ${solveInput(input,2)}`);
